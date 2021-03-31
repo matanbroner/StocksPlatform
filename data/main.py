959e8136d4444959720fd8385977cfeae98b3548
Matan Broner <matanbroner@gmail.com>
2021-03-31 11:27:21 -0700

    client init, ngninx init, compose file

--- data/main.py
+++ data/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def health_check():
-    return 'OK'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def health_check():
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
