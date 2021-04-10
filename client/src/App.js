4191eba1f8e1bd433e7ed4329c145274bc59613a
Nathaniel Chang <namachan@ucsc.edu>
2021-04-10 11:33:15 -0700

    created static homepage w/ login and signup buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Header, Segment, Button, Image } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
 
-function App() {
+function Logo() {
+  return (
+    <div className="Logo">
+      Stocked (Logo)
+    </div>
+  )
+}
+
+function Message() {
+  return (
+    <Header size="large">
+      <div className="HomeMessage">
+        Welcome to the Site. Come check out our stuff!
+      </div>
+    </Header>
+  )
+}
+
+function SignUpButton() {
+  return (
+    <div className="Signup">
+      <Button basic inverted color="green">Sign Up</Button>
+    </div>
+  )
+}
+
+function LogInButton() {
+  return (
+    <div className="Login">
+      <Button basic inverted color="green">Log In</Button>
+    </div>
+  )
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="HomeBackground">
+        <Header className="Header" as='h2'>
+          <Logo />
+          <Message />
+          <div className="Buttons">
+            <SignUpButton />
+            <LogInButton />
+          </div>
+        </Header>
+      </div>
     </div>
   );
 }
