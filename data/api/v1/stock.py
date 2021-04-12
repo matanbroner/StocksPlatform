a0c02851529c08809d048b5f377712c75fbcb73e
Matan Broner <matan@fiddler.ai>
2021-04-12 11:44:43 -0700

    v1-api changes

--- data/api/v1/stock.py
+++ data/api/v1/stock.py
@@ -5,11 +5,13 @@ from api import json_response
 
 router = Blueprint('stock_router', __name__)
 
-@stock_router.route('/', methods=["GET"])
-def get_all_stocks():
+@stock_router.route('/', defaults={'stock_id': None}, methods=["GET"])
+@stock_router.route('/<stock_id>', methods=["GET"])
+def get_stock(stock_id):
     session = create_session()
     try:
-        stocks = session.query(Stock).all()
+        if stock_id:
+            stocks = session.query(Stock).filter()
         return json_response(
             status_code=200,
             data=stocks
