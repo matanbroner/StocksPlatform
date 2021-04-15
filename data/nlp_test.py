a127d814da92fcfe8b01c5fe7022539a06d65963
AJ Wong <ajwong408@gmail.com>
2021-04-15 02:23:35 -0700

    fixed wording on arg -s

--- data/nlp_test.py
+++ data/nlp_test.py
@@ -4,7 +4,7 @@ from nlp.sentiment_data import main
 parser = argparse.ArgumentParser(description='NLP Test File')
 parser.add_argument('-k', metavar='key', help='FMP API Key',
                     type=str, required=True)
-parser.add_argument('-s', metavar='stock', help='Stock ticker list',
+parser.add_argument('-s', metavar='stock_ticker', help='Stock ticker list',
                     type=list, nargs='+', default=['AAPL', 'GME', 'TSLA'])
 args = parser.parse_args()
 
