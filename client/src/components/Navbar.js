367cf4870d510bc9f04086d8f25f4e48ecc28927
Nathaniel Chang <namachan@ucsc.edu>
2021-04-17 19:44:33 -0700

    Added state to login form.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,28 +19,28 @@ class Navbar extends Component {
                     >
                     
                     <Link to='/dashboard/home'>
-                        <Menu.Item as='a'>
+                        <Menu.Item as='div'>
                             <Icon name='home' />
                             Home
                         </Menu.Item>
                     </Link>
 
                     <Link to='/dashboard/projects'>
-                        <Menu.Item as='a'>
+                        <Menu.Item as='div'>
                             <Icon name='users' />
                             Projects
                         </Menu.Item>
                     </Link>
 
                     <Link to='/dashboard/stock-search'>
-                        <Menu.Item as='a'>
+                        <Menu.Item as='div'>
                             <Icon name='chart line' />
                             Stock Search
                         </Menu.Item>
                     </Link>
 
                     <Link to='/dashboard/settings'>
-                        <Menu.Item as='a'>
+                        <Menu.Item as='div'>
                             <Icon name='settings' />
                             Settings
                         </Menu.Item>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,50 +1,75 @@
 import React, { Component } from 'react';
 import '../App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Form, Button, Header} from 'semantic-ui-react';
+import { Form, Header, Checkbox, Grid, Container } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-class SignupForm extends Component {
+class Signup extends Component {
+    handleSubmit = () => {
+        const data = {
+            first_name: this.firstName,
+            last_name: this.lastName,
+            user_name: this.userName,
+            email: this.email,
+            password: this.password,
+        }
+        console.log(data);
+
+        axios.post('/signup', data).then(
+            res => {
+                console.log(res);
+            }
+        ).catch(
+            err => {
+                console.log(err);
+            }
+        )
+    };
+
     render() {
         return (
-            <Form className='signup-form'>
-                <Form.Group widths='equal'>
-                    <Form.Input
-                        fluid
-                        id='form-subcomponent-shorthand-input-first-name'
-                        label='First name'
-                        placeholder='First name'
-                    />
-                    <Form.Input
-                        fluid
-                        id='form-subcomponent-shorthand-input-last-name'
-                        label='Last name'
-                        placeholder='Last name'
-                    />
-                </Form.Group>
-                {/* Add error messages later to check for invalid requests */}
-                <Form.Input label='Username' className='signup-user' placeholder='Username' />
-                <Form.Input label='Email' className='signup-email' placeholder='E-mail Address' />
-                <Form.Input label='Password' className='signup-password' placeholder='Password' />
-                
-                <Link to='/dashboard/home'>
-                    <Button className='signup-button'>Create Account</Button>
-                </Link>
-            </Form>
+            <Container>
+                <div className="signup">
+                    <Grid className="center aligned">
+                        <Form onSubmit={ this.handleSubmit } className='signup-form' >
+                            <Header size='huge' className='SignupTitle'>
+                                Sign Up
+                            </Header>
+
+                            <Form.Group>
+                                <Form.Field>
+                                    <label>First Name</label>
+                                    <input placeholder='First Name' 
+                                    onChange={e => this.firstName = e.target.value}/>
+                                </Form.Field>
+                                <Form.Field>
+                                    <label>Last Name</label>
+                                    <input placeholder='Last Name' 
+                                    onChange={e => this.lastName = e.target.value}/>
+                                </Form.Field>
+                            </Form.Group>
+
+                            {/* Add error messages later to check for invalid requests */}
+                            <Form.Input label='Username' className='signup-user' placeholder='Username' 
+                            onChange={e => this.userName = e.target.value}/>
+                            <Form.Input label='Email' className='signup-email' placeholder='E-mail Address'
+                            onChange={e => this.email = e.target.value}/>
+                            <Form.Input label='Password' className='signup-password' placeholder='Password' 
+                            onChange={e => this.password = e.target.value}/>
+                            <Form.Field>
+                                <Checkbox label='I agree to the Terms and Conditions'></Checkbox>
+                            </Form.Field>
+                            
+                            {/* <Link to='/dashboard/home'> */}
+                                <Form.Button className="signup-button" content="Create Account" />
+                            {/* </Link> */}
+                        </Form>
+                    </Grid>
+                </div>
+            </Container>
         )
     }
-}
-
-function Signup() {
-    return (
-        <div>
-            <Header size='large' className='SignupTitle'>
-                Sign Up
-            </Header>
-
-            <SignupForm />
-        </div>
-    )
 }
 
 export default Signup;
