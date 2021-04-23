431b0db14e11e3c387afa26e7a7981e9ddcbd913
AJ Wong <ajwong408@gmail.com>
2021-04-22 22:52:01 -0700

    changed command line argument to unittest

--- data/nlp_test.py
+++ data/nlp_test.py
@@ -1,20 +1,46 @@
 import argparse
+import unittest
 from nlp.sentiment_data import main
-
-parser = argparse.ArgumentParser(description='NLP Test File')
-parser.add_argument('-k', metavar='key', help='FMP API Key',
-                    type=str, required=True)
-parser.add_argument('-s', metavar='stock_ticker', help='Stock ticker list',
-                    type=list, nargs='+', default=['AAPL', 'GME', 'TSLA'])
-args = parser.parse_args()
-
-stock_list = list(map(''.join, args.s))
-
-main(args.k, stock_list)
+import nlp.news_sources as news
+import nlp.nlp as nlp
 
 # # TODO: remove hardcoded API key and use env variable
-# API_KEY = "e2d9d13e5b7ac7976216133f32d7f775"
+API_KEY = "e2d9d13e5b7ac7976216133f32d7f775"
 
-# class TestNaturalLanguageProcessing:
-#     def test_retrieve_stock_data(self, stock_list=['AAPL', 'GME', 'TSLA']):
-#         assert(main(API_KEY, stock_list) == 1)
+
+class TestSentimentData(unittest.TestCase):
+    def test_news_pipeline(self, stock_list=['AAPL', 'GME', 'TSLA']):
+        self.assertEqual(main(API_KEY, stock_list), 1)
+
+class TestNewsSources(unittest.TestCase):
+    def setUp(self):
+        self.general_news = news.GeneralNewsData(API_KEY, 'AAPL')
+        #self.reddit_news = news.RedditData(client_id, client_secret, subreddit)
+
+    def test_get_stock(self):
+        self.assertEqual(self.general_news.get_stock(), 'AAPL')
+
+    def test_retrieve_data(self):
+        self.assertEqual(list(self.general_news.retrieve_data().columns), ['stock', 'source', 'date', 'title', 'content'])
+       
+class TestNLP(unittest.TestCase):
+    def setUp(self):
+        pass
+
+def run_tests():
+    test_classes = [TestSentimentData, TestNewsSources, TestNLP]
+
+    loader = unittest.TestLoader()
+
+    suites_list = []
+    for test_class in test_classes:
+        suite = loader.loadTestsFromTestCase(test_class)
+        suites_list.append(suite)
+
+    big_suite = unittest.TestSuite(suites_list)
+
+    runner = unittest.TextTestRunner()
+    results = runner.run(big_suite)
+
+if __name__ == '__main__':
+    run_tests()
