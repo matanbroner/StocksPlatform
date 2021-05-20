fffc828c1308599887c51c4af8a1482808169a14
AJ Wong <ajwong408@gmail.com>
2021-05-20 11:30:14 -0700

    finished tests for pipeline

--- data/tests/nlp_test.py
+++ data/tests/nlp_test.py
@@ -1,11 +1,38 @@
 import unittest
+
+import datetime
+import pandas as pd
+
 from nlp.retrieve_news import main
 import nlp.news_sources as news
 
 from db.sqlalchemy_db import init_db_connection
 
+from db.handlers.stock_handler import (
+    create_stock,
+    delete_stock_by_id
+)
+
+from db.handlers.news_source_handler import (
+    create_news_source,
+    delete_news_source_by_id
+)
+
+from db.handlers.news_articles_handler import (
+    get_news_article_by_id,
+    create_news_article,
+    delete_news_article_by_id
+)
+
+from nlp.nlp_pipeline import (
+    filter_news,
+    pre_process,
+    determine_sentiment,
+    save_data
+)
+
 # # TODO: remove hardcoded API key and use env variable
-API_KEY = "fea39208b1f255ac4d0fa3a901585172"
+API_KEY = "0cc8ff319abbabcab8e06288e3c67c3c"
 
 class TestNewsSources(unittest.TestCase):
     def setUp(self):
@@ -23,17 +50,68 @@ class TestNewsSources(unittest.TestCase):
         self.assertEqual(list(self.general_news.retrieve_data().columns), ['stock', 'source', 'date', 'title', 'content'])
 
 class TestNLPPipeline(unittest.TestCase):
-    def test_nlp_pipeline(self, stock_list=['AAPL', 'TSLA', 'GME', 'AMZN', 'BB', 'DIS', 'MSFT', 'TWTR', 'SNAP', 'NFLX', 'SBUX']):
-        self.assertEqual(main(API_KEY, stock_list), 1)
+    def setUp(self):
+        self.test_df = pd.DataFrame([['TEST_STOCK', 'TEST_SOURCE', datetime.datetime(1972, 5, 17), "This is a test headline.", "This is test content. Usually there is a lot more text.", 1.0]],
+                               columns=['stock', 'source', 'date', 'title', 'content', 'sentiment'])
 
-    def test_pipeline_stress(self):
-        stock_list = []
-        for i in range(1000):
-            stock_list.append('AAPL')
+        self.stock = create_stock('TEST_STOCK')
+        self.source = create_news_source('TEST_SOURCE')
+
+    def tearDown(self):
+        delete_stock_by_id(self.stock['id'])
+        delete_news_source_by_id(self.source['id'])
+
+    def test_filter_news(self):
+        article = create_news_article(
+            self.source['id'],
+            self.stock['id'],
+            self.test_df['sentiment'][0],
+            self.test_df['title'][0],
+            datetime.datetime(1972, 5, 17))
+
+        filtered_df = filter_news(self.test_df)
+        self.assertTrue(filtered_df.empty)
+
+        delete_news_article_by_id(article['id'])
+
+    def test_pre_process(self):
+        docs = pre_process(iter(self.test_df['title']))
+        text_list = []
+        for doc in docs:
+            text_list.append(' '.join([token.text for token in doc]))
+
+        self.assertGreaterEqual(len(self.test_df['title'][0]), len(text_list[0]))
+
+    def test_determine_sentiment(self):
+        headlines = ["This should be positive", 
+        "This should be neutral.", 
+        "This should be negative."]
+        docs = pre_process(headlines)
+        sentiment_list = determine_sentiment(docs)
+        self.assertGreater(sentiment_list[0], 0)
+        self.assertEqual(sentiment_list[1], 0)
+        self.assertLess(sentiment_list[2], 0)
+
+    def test_save_data(self):
+        article = save_data(self.test_df)
+        self.assertIsNotNone(get_news_article_by_id(article['id']))
+        delete_news_article_by_id(article['id'])
+
+    def test_full_pipeline(self, stock_list=['AAPL', 'TSLA', 'GME', 'AMZN', 'BB', 'DIS', 'MSFT', 'TWTR', 'SNAP', 'NFLX', 'SBUX']):
         self.assertEqual(main(API_KEY, stock_list), 1)
 
     def test_pipeline_no_crash_on_bad_src(self, stock_list=['XXXX']):
         self.assertEqual(main(API_KEY, stock_list), 1)
+
+    # def test_pipeline_stress(self):
+    #     """
+    #     Outdated test. Pipeline does not process repeat articles anymore, so this form of test is no longer effective.
+    #     """
+    #     stock_list = []
+    #     for i in range(1000):
+    #         stock_list.append('AAPL')
+    #     self.assertEqual(main(API_KEY, stock_list), 1)
+
 
 if __name__ == '__main__':
     init_db_connection()
